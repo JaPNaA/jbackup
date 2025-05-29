8169f3c7b7b93206a1ee00403b55785694633194
JaPNaA <japnaa@japnaa.dev>
2025-05-29 00:57:31 -0400

    Implement init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::{env, fs, io};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world!!!");
+    let mut args_iter = env::args();
+    args_iter.next(); // ignore path
+
+    let command = args_iter.next().unwrap_or_default();
+
+    let result = match command.as_str() {
+        "" => Err("Error: no command specified".to_string()),
+        "init" => match init_repo() {
+            Err(error) => Err(format!("Failed to initalize repository: IO Error: {error}")),
+            Ok(_) => Ok(()),
+        },
+        _ => Err(format!("Error: unknown command '{}'", command)),
+    };
+
+    match result {
+        Err(error) => {
+            println!("Fatal: {}", error);
+        }
+        Ok(_) => (),
+    }
+}
+
+fn init_repo() -> io::Result<()> {
+    fs::create_dir(".jbackup")?;
+    fs::write(".jbackup/branches", "main\tNULL")?;
+    fs::write(".jbackup/head", "NULL")?;
+    Ok(())
 }
